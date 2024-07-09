8f3610fa3863017ced53fc4d8501aab9ae3eb24b
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-09 07:48:11 +0300

    Deleted unnecessary variable 'data'

--- index.ts
+++ index.ts
@@ -19,16 +19,11 @@ async function askForStopCode(): Promise<string> {
 async function getDataForStopPoints(code: string): Promise<JsonBus[]> {
     try {
         const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${code}/Arrivals/?app_key=0751e7d29b944370b4ad1378bb1c3f66`);
-        const data = await response.json();
+        const busses: JsonBus[]  = await response.json();
 
-        const busses: JsonBus[] = data;
         busses.sort(Sorter.sortByArrivalTime);
 
-        //to be considered, moving this as a separate function
-        let firstBusses: JsonBus[] = new Array();
-        for (let i = 0; i < busses.length && i < 5; i++) {
-            firstBusses.push(busses[i]);
-        }
+        let firstBusses: JsonBus[] = busses.slice(0,5);
         return firstBusses;
     } catch (error: any) {
         console.error(error)
